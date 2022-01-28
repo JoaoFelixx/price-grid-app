aedbf27eb04b707517e18f9b8e0d3a45f037b876
Felix developer <felixxdeveloper@gmail.com>
2022-01-28 13:39:44 -0300

    Better the code at navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,23 @@
 import {
+  Search,
   Image,
   Input,
+  Div,
   Nav,
 } from './style';
+import { BsSearch } from 'react-icons/bs';
+
 
 function Navbar() {
   return (
     <Nav>
       <Image src="./favicon.png" alt="Mercado Privado ia" />
-      <Input placeholder='Buscar produtos, marcas e muito mais' type='search' />
+      <Div>
+        <Input placeholder='Buscar produtos, marcas e muito mais' type='search' />
+        <Search>
+          <BsSearch />  
+        </Search>
+      </Div>
     </Nav>
   )
 }
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Nav = styled.nav`
   grid-area: navbar;
+  padding: 4px;
   background-color: #FFF159;
   width: 100%;
   display: flex;
@@ -12,17 +13,42 @@ const Nav = styled.nav`
   @media (max-width: 768px) { justify-content: center;}
 `;
 
+const Div = styled.div`
+  background-color: #000;
+  width: 70%;
+
+  @media (max-width: 768px) { width: 94%; }
+`;
+
 const Input = styled.input`
   background-color: #ffffff;
   height: 35px;
   font-size: 16px;
   padding: 25px 6px 25px 6px;
   border: none;
-  width: 94%;
+  width: 90%;
 
-  @media (max-width: 768px) { font-size: 12px; }
+  @media (max-width: 768px) { 
+    font-size: 12px; 
+    padding: 0; 
+  }
 `;
 
+const Search = styled.button`
+  background-color: #fff;
+  color: #000;
+  width: 10%;
+  height: 35px;
+  font-size: 16px;
+  padding: 15px 6px 35px 6px;
+  cursor: pointer;
+  border: none;
+
+  @media (max-width: 768px) { 
+    font-size: 12px; 
+    padding: 0;
+  }
+`; 
 
 const Image = styled.img`
   float: left;
@@ -30,7 +56,9 @@ const Image = styled.img`
 `;
 
 export {
+  Search,
   Image,
   Input,
+  Div,
   Nav,
 }
