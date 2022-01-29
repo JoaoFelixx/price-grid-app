471bdccddfdac253f653a5537833bbe87f306da6
Felix developer <felixxdeveloper@gmail.com>
2022-01-29 12:51:06 -0300

    Finished the project

--- src/components/PopularCategories/index.js
+++ src/components/PopularCategories/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
 import Index from '../Index';
 import {
 	Div,
 	Flex,
 	Card,
+	Text,
 } from './style';
 import {
 	AiFillCar,
@@ -17,26 +17,64 @@ import {
 import {
 	BiFootball,
 } from 'react-icons/bi';
+import {
+	GiLipstick,
+	GiSteeringWheel,
+	GiGamepad,
+} from 'react-icons/gi';
+import {
+	RiBearSmileFill
+} from 'react-icons/ri';
+
 
 function PopularCategories (argument) {
 	return (
 		<Div>
-			<Index title='Categorias populares' />
+			<Index title='Categorias populares' /> <br />
 			<Flex>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillSkin style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillMobile style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <BsTv style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
+				<Card> 
+					<AiFillCar style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Carros, motos e outros </Text>
+				</Card>
+				<Card> 
+					<GiLipstick style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Beleza e cuidado pessoal </Text>
+				</Card>
+				<Card> 
+					<AiFillSkin style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Calçados, roupas e bolsas </Text>
+				</Card>
+				<Card> 
+					<AiFillMobile style={{ color: '#2374E1', fontSize: '50px' }} />
+					<Text> Celulares e telefones </Text>
+				</Card>
+				<Card> 
+					<BsTv style={{ color: '#2374E1', fontSize: '50px' }}/> 
+					<Text> Informática </Text>
+				</Card>
 			</Flex>
+
 			<Flex>
-				<Card> <BsFillHouseFill style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <BiFootball style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
-				<Card> <AiFillCar style={{ color: '#2374E1', fontSize: '50px' }}/> </Card>
+				<Card> 
+					<BsFillHouseFill style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Imóveis </Text>
+				</Card>
+				<Card> 
+					<BiFootball style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Esportes e fitness </Text>
+				</Card>
+				<Card> 
+					<RiBearSmileFill style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Brinquedos e Hobbies </Text>
+				</Card>
+				<Card> 
+					<GiGamepad style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Games </Text>
+				</Card>
+				<Card> 	
+					<GiSteeringWheel style={{ color: '#2374E1', fontSize: '50px' }} /> 
+					<Text> Acessórios de veículos </Text>
+				</Card>
 			</Flex>
 		</Div>
 	)
--- src/components/PopularCategories/style.js
+++ src/components/PopularCategories/style.js
@@ -8,24 +8,30 @@ const Div = styled.div`
 
 const Flex = styled.div`
 	width: 100%;
-	background-color: red;
 	display: flex;
 	flex-direction: row;
-
-	@media (max-width: 768px) { flex-direction: column; }
 `;
 
 const Card = styled.div`
-	background-color: black;
+	background-color: #fff;
+	margin: 1px;
+	padding: 10px;
+	text-align: center;
 	width: 100%;
 	height: 100px;
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+`;
+
+const Text = styled.p`
+	
 `;
 
 export {
 	Div,
 	Flex,
 	Card,
+	Text,
 }
